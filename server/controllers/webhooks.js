84386bc4e4a4ee2d9f0686cd6b5e062907eda2ae
POORVAJA-V <poorvajavijayan16@gmail.com>
2025-06-19 16:29:48 +0530

    name issues sorted

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -18,7 +18,7 @@ export const clerkWebhooks = async (req, res) => {
         const userData = {
           _id: data.id,
           email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
+         name: `${data.first_name || ''} ${data.last_name || ''}`.trim(),
           imageUrl: data.image_url,
         };
         await User.create(userData);
