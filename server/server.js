69dbad62efcab4b443b9023acfb2986443a9e2b9
POORVAJA-V <poorvajavijayan16@gmail.com>
2025-06-19 17:08:38 +0530

    clerk

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
@@ -11,10 +13,12 @@ await connectDB();
 
 // Middleware
 app.use(cors());
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req, res) => res.send("API WORKING"));
 app.post('/clerk',express.json(),clerkWebhooks)
+app.use('/api/educator',express.json(),educatorRouter)
 
 // Server
 const PORT = process.env.PORT || 5000;
